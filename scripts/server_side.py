1bb8caf59917d0ad791b093a1e9948e935348796
haythem-cpu <56981607+haythem-cpu@users.noreply.github.com>
2020-05-31 15:39:35 +0100

    back_end

---
+++ scripts/server_side.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,request
+import program_manger as pm
+
+
+app=Flask(__name__,template_folder="../web_page")
+obj=pm.create_table()
+
+@app.route('/')
+
+def settings():
+    return render_template("first.html")
+
+@app.route('/create',methods=['POST'])
+
+def create():
+    start=request.form["start"]
+    obj.file_name=request.form["name"]
+    obj.number=int(request.form["day_number"])
+    obj.start_hour=int(request.form["wakeup"])
+    obj.end_hour=int(request.form["sleep"])
+    obj.duration=int(request.form["sleep"])-int(request.form["wakeup"])
+    obj.date_system(start)
+    obj.create_data()
+    data_table={"table":obj.data,"num":obj.number,"duration":obj.duration}
+    return render_template("index.html",table=data_table)
+
+@app.route('/pdf_create',methods=['POST'])
+
+def made():
+    for i in range(1,obj.number+1):
+        for l in range(1,obj.duration+1):
+            obj.data[l][i][0]=request.form[str(l)+str(i)+"time"]
+            obj.data[l][i][1]=int(request.form[str(l)+str(i)+"duration"])
+    obj.web2pdf()
+    return render_template("final_page.html")
+
+if __name__=="__main__":
+    app.run(debug=True,port=4000)
